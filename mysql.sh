a3d7f2e22c5dcf3357dd81c3a7e6def89506a4c7
Lerte Smith <lerte@qq.com>
2018-09-04 18:48:38 +0800

    change mysql password
    Signed-off-by: Lerte Smith <lerte@qq.com>

--- mysql.sh
+++ mysql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v ${PWD}/mysql/conf:/etc/mysql/conf.d -v ${PWD}/mysql/data:/var/lib/mysql -d mysql
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=usbw -v ${PWD}/mysql/conf:/etc/mysql/conf.d -v ${PWD}/mysql/data:/var/lib/mysql -d mysql
